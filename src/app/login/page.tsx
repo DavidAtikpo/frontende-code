176a535382b04b7ab7f40e85718826116b647671
Faust Oswald <114805447+Oswald-Faust@users.noreply.github.com>
2024-11-19 16:13:14 +0100

    Adding login and signup logic with components usage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,10 @@
+import AuthPage from "../components/auth";
+
 export default function Login() {
     return (
       <div>
-        <h1>Connexion</h1>
-        <form>
-          <label>Email :</label>
-          <input type="email" name="email" />
-          <label>Mot de passe :</label>
-          <input type="password" name="password" />
-          <button type="submit">Se connecter</button>
-        </form>
+        <AuthPage></AuthPage>
+       
       </div>
     );
   }
-  
