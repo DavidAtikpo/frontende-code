159dcdffc6aea58047d0a1a853306792b9a11a4f
DavidAtikpo <davidatikpo44@gmail.com>
2024-12-05 16:15:37 +0000

    changement du titre

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.png" />
-        <title>Votre Application</title>
+        <title>DUBON SERVICE</title>
         <meta name="description" content="Découvrez Dubon Services Event : votre marketplace pour des produits frais, congelés, formations spécialisées, et services événementiels. Simplifiez votre quotidien dès aujourd'hui !"/>
 
       </head>
