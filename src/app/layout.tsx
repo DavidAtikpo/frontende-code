4197dd9ee149876da51b0b911d493f2bb217cebe
DavidAtikpo <davidatikpo44@gmail.com>
2024-12-19 07:08:12 +0000

    meta donnee

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <title>DUBON SERVICE</title>
+        <title>DUBON SERVICES</title>
         <meta name="description" content="Découvrez Dubon Services Event : votre marketplace pour des produits frais, congelés, formations spécialisées, et services événementiels. Simplifiez votre quotidien dès aujourd'hui !"/>
       </head>
       <body>
